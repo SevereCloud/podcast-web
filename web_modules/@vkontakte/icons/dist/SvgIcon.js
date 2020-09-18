83c3580d56808fa80c4a60380ed34a7b55e88872
VChertukhin <VChertukhin@users.noreply.github.com>
2020-09-18 11:18:27 +0000

    deploy: 4b53aeb9fab2cc8f2d9ce08405506ab68db58f43

---
+++ web_modules/@vkontakte/icons/dist/SvgIcon.js
@@ -0,0 +1,9 @@
+import '../../../common/_commonjsHelpers-6f5dde0d.js';
+import '../../../common/index-df564204.js';
+import { S as SvgIcon_1 } from '../../../common/SvgIcon-3febc6b1.js';
+
+
+
+var SvgIcon = SvgIcon_1.SvgIcon;
+export { SvgIcon };
+//# sourceMappingURL=SvgIcon.js.map
